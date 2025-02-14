e9d0f73057b950c471bdc99b692226387a42cee8
Anna Marin B. Crisolo <marin@Annas-MacBook-Air.local>
2025-02-14 15:14:56 +0800

    Notifcation Fade

---
+++ app/javascript/controllers/_toast_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus";
+import { Toast } from "bootstrap";
+
+export default class extends Controller {
+  connect() {
+    const toasts = this.element.querySelectorAll(".toast");
+    toasts.forEach((toast) => {
+      let bsToast = new Toast(toast, { autohide: true, delay: 3000 });
+      
+      // Add event listeners for debugging
+      toast.addEventListener('shown.bs.toast', () => {
+        console.log('Toast is shown');
+      });
+      
+      toast.addEventListener('hidden.bs.toast', () => {
+        console.log('Toast is hidden');
+      });
+      
+      bsToast.show();
+    });
+  }
+}
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,20 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-import NotificationsController from "./notifications_controller"    
+import ToastController from "./_toast_controller"    
 
 const application = Application.start()
-application.register("notifications", NotificationsController)
+application.register("toast", ToastController)
 
 export { application }
 
 
-document.addEventListener("turbo:load", function() {
-    setTimeout(() => {
-        document.querySelectorAll('.toast').forEach(function(toastElement) {
-            let toast = new bootstrap.Toast(toastElement, {
-                autohide: true,  // Enables auto-hide
-                delay: 2000      // Hides the toast after 2 seconds
-            });
-            toast.show();
-        });
-    }, 500); // Small delay before showing to avoid conflicts
-});
